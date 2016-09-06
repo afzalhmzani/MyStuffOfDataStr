f4f4aaa906b1192e001d062c5d56ffc9c3caf9f2
Abdullah Alhmzani <abdullah.alhmzani@gmail.com>
2016-09-06 05:29:15 +0300

    Print Hello World

--- MyStuffOfDataStr/Program.cs
+++ MyStuffOfDataStr/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace MyStuffOfDataStr
+﻿using System;
+
+namespace MyStuffOfDataStr
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            System.Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
