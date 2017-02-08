7a7f09fca8bc7db318c6f784433973c2058d7e16
Abdullah Alhmzani <abdullah.alhmzani@gmail.com>
2017-02-08 22:14:17 +0300

    adding Circular linked list

---
+++ CircularLinkedListProject/CircularLinkedList.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace CircularLinkedListProject
+{
+    class CircularLinkedList
+    {
+        private Node last;
+
+        public CircularLinkedList()
+        {
+            last = null;
+        }
+
+        public void DisplayList()
+        {
+            if (last == null)
+            {
+                Console.WriteLine("List is empty\n");
+                return;
+            }
+            Node p = last.link;
+
+            do
+            {
+                Console.Write(p.info + " ");
+                p = p.link;
+            } while (p != last.link);
+
+            Console.WriteLine();
+        }
+
+        public void InsertInBeginning(int data)
+        {
+            Node temp = new Node(data);
+            temp.link = last.link;
+            last.link = temp;
+        }
+
+        public void InsertInEmptyList(int data)
+        {
+            Node temp = new Node(data);
+            last = temp;
+            last.link = temp;
+        }
+
+        public void InsertAtEnd(int data)
+        {
+            Node temp = new Node(data);
+            temp.link = last.link;
+            last.link = temp;
+            last = temp;
+
+        }
+
+        public void CreatList()
+        {
+            int i, n, data;
+
+            Console.Write("Enter number of node: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 0)
+                return;
+            Console.Write("Enter the element to be iserted: ");
+            data = Convert.ToInt32(Console.ReadLine());
+            InsertInEmptyList(data);
+
+            for (i = 2; i < n; i++)
+            {
+                Console.Write("Enter the elemnt to be inserted: ");
+                data = Convert.ToInt32(Console.ReadLine());
+                InsertAtEnd(data);
+            }
+        }
+
+        public void InsertAfter(int data, int x)
+        {
+            Node p = last.link;
+            do
+            {
+                if (p.info == x)
+                    break;
+                p = p.link;
+            } while (p != last.link);
+            if (p == last.link && p.info != x)
+                Console.WriteLine(x + "the elemnt is not found");
+            else
+            {
+                Node temp = new Node(data);
+                temp.link = p.link;
+                p.link = temp;
+                if (p == last)
+                    last = temp;
+            }
+
+        }
+
+    }
+}
---
+++ CircularLinkedListProject/CircularLinkedListDemo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CircularLinkedListProject
+{
+    class CircularLinkedListDemo
+    {
+        static void Main(string[] args)
+        {
+            int choice, data, x;
+
+            CircularLinkedList CList = new CircularLinkedList();
+
+            CList.CreatList();
+
+            while (true)
+            {
+                Console.WriteLine("1. Display list");
+                Console.WriteLine("2. Insert in empty list");
+                Console.WriteLine("3. Insert in the beginning");
+                Console.WriteLine("4. Insert in the end");
+                Console.WriteLine("5. Insert after a node");
+                Console.WriteLine("6. Delete first node");
+                Console.WriteLine("7. Delete last node");
+                Console.WriteLine("8. Delete any node");
+                Console.WriteLine("9. Quit");
+
+                Console.Write("Enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 9)
+                    break;
+                switch (choice)
+                {
+                    case 1:
+                        CList.DisplayList();
+                        break;
+                    case 2:
+                        Console.Write("Enter the elemint to be inserted: ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        CList.InsertInEmptyList(data);
+                        break;
+                    case 3:
+                        Console.Write("Enter the elemnt to be inserted: ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        CList.InsertInBeginning(data);
+                        break;
+                    case 4:
+                        Console.Write("Enter the elemint to be inserted: ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        CList.InsertAtEnd(data);
+                        break;
+                    case 5:
+                        Console.Write("Enter the emint to be inserted: ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter the elemnt after which to be inserted: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        CList.InsertAfter(data, x);
+                        break;
+
+
+                }
+
+            }
+        }
+    }
+}
