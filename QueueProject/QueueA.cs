cc5734569401e7529b2c5f70b3e22a8471cb6756
afzalhmzani <abdullah.alhmzani@gmail.com>
2017-02-16 02:27:35 +0300

    Complet array insid queue

--- QueueProject/QueueA.cs
+++ QueueProject/QueueA.cs
@@ -1,12 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QueueProject
 {
     class QueueA
     {
+        private int[] queueArray;
+        private int front;
+        private int rear;
+
+        public QueueA()
+        {
+            queueArray = new int[10];
+            front = -1;
+            rear = -1;
+        }
+
+        public QueueA(int maxIndex)
+        {
+            queueArray = new int[maxIndex];
+            front = -1;
+            rear = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return (front == -1 || front == rear + 1);
+        }
+
+        public bool IsFull()
+        {
+            return (rear == queueArray.Length - 1);
+        }
+
+        public int Size()
+        {
+            if (IsEmpty())
+                return 0;
+            else
+            {
+                return rear - front + 1;
+            }
+        }
+
+        public void Insert(int x)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Queue Overflow\n");
+                return;
+            }
+            if (front == -1)
+                front = 0;
+            rear = rear + 1;
+            queueArray[rear] = x;
+        }
+
+        public int Delete()
+        {
+            int x;
+            if (IsEmpty())
+                throw new System.InvalidOperationException("Queue underflow");
+            x = queueArray[front];
+            front = front + 1;
+            return x;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+                throw new System.InvalidOperationException("Queue underflow");
+            return queueArray[front];
+        }
+
+        public void Display()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty\n");
+                return;
+            }
+            Console.WriteLine("Queue is: \n\n");
+            for (int i = front; i <= rear; i++)
+                Console.Write(queueArray[i] + " ");
+            Console.WriteLine();
+        }
     }
 }
--- QueueProject/QueueDemo.cs
+++ QueueProject/QueueDemo.cs
@@ -1,9 +1,55 @@
-﻿namespace QueueProject
+﻿using System;
+
+namespace QueueProject
 {
     class QueueDemo
     {
         static void Main(string[] args)
         {
+            int choice, x;
+            QueueA queueA = new QueueA(10);
+
+            while (true)
+            {
+                Console.WriteLine("1. Insert an element on the queue.");
+                Console.WriteLine("2. Delete an element from the queue.");
+                Console.WriteLine("3. Display the top element in the queue.");
+                Console.WriteLine("4. Display all element in the queue. ");
+                Console.WriteLine("5. Display size of the queue.");
+                Console.WriteLine("6. Quit.");
+                Console.Write("Enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 6)
+                    break;
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter the elemnt to be pushed: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        queueA.Insert(x);
+                        break;
+                    case 2:
+
+                        x = queueA.Delete();
+                        Console.WriteLine("Deleted element is: " + x);
+                        break;
+                    case 3:
+                        Console.WriteLine("The element at the top is " + queueA.Peek());
+                        break;
+                    case 4:
+
+                        queueA.Display();
+                        break;
+                    case 5:
+                        Console.WriteLine("The size of the stack is: " + queueA.Size());
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice");
+                        break;
+                }
+                Console.WriteLine(" ");
+            }
         }
     }
 }
